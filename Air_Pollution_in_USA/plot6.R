2a76c572205730fc2959a460c39033d6c4b4fffb
oDoraemon <ylchen.xd@qq.com>
2015-11-19 17:37:49 +0800

    add the change rate to the last 3 year plot

--- Air_Pollution_in_USA/plot6.R
+++ Air_Pollution_in_USA/plot6.R
@@ -27,8 +27,10 @@ motorSub1 <- subset(LA, LA$SCC %in% sccSub)
 motorEmission0 <- with(motorSub0, tapply(Emissions, year, sum))
 motorEmission1 <- with(motorSub1, tapply(Emissions, year, sum))
 
+
 # plot
-png(filename = "plot6.png", width = 480, height = 480, units = "px")
+png(filename = "plot6.png", width = 860, height = 480, units = "px")
+par(mfrow = c(1,2), mar = c(5,4,2,1))
 
 plot(seq(1999,2008,3),motorEmission0, main = "Motor vehicle Emission in Baltimore & LA",pch = 19, col = "steelblue", xlab = "year", ylab = "Emission (*ton)", xlim = c(1998, 2009),ylim = c(0,11000))
 points(seq(1999,2008,3),motorEmission1, pch = 19,col = "red")
@@ -40,5 +42,28 @@ text(seq(1999,2008,3) + 0.5,motorEmission0 + 100, round(motorEmission0,0))
 text(seq(1999,2008,3) + 0.5,motorEmission1 - 100, round(motorEmission1,0))
 
 legend("right", lty=1, lwd = 2, col = c("red", "blue"), legend = c("LA", "Baltimore"))
+
+# the rate change compare
+getrate <- function(x) {
+  rate = list()
+  for (i in 2:length(x))
+    rate = c(rate,(x[i] - x[i - 1])/x[i - 1])
+  rate
+}
+
+# the rate plot
+rate0 = unlist(getrate(motorEmission0))
+rate1 = unlist(getrate(motorEmission1))
+
+plot(seq(2002,2008,3),rate0, main = "Motor vehicle Emission rate in Baltimore & LA",pch = 19, col = "steelblue", xlab = "year", ylab = "rate to last 3year", xlim = c(2000, 2009), ylim = c(-1, 1))
+lines(seq(2002,2008,3),rate0,lwd = 2, col = "steelblue")
+
+
+points(seq(2002,2008,3),rate1, pch = 19, col = "red")
+lines(seq(2002,2008,3),rate1,lwd = 2, col = "red")
+
+text(seq(2002,2008,3) - 0.5,rate0, round(rate0,2), col = "steelblue")
+text(seq(2002,2008,3) + 0.5,rate1, round(rate1,2), col = "red")
+abline(h=0,lty = 2)
 
 dev.off()
